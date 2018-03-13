438c562aec0b8765afc5649938622b43551a4759
TheWarpyre <Oliver@thewarpyre.com>
2018-03-13 13:22:26 +0100

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,84 @@
+<?php
+//including the database connection file
+include_once("config.php");
+
+//fetching data in descending order (lastest entry first)
+//$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
+?>
+
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="./favicon.ico">
+  <link rel="stylesheet" href="./css/style.css">
+<link href="./css/bootstrap.min.css" rel="stylesheet">
+	<title>Homepage</title>
+</head>
+
+<body>
+  <div class="collapse navbar-collapse" id="navbars">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="javascript:void(0);" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown01">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<main role="main" class="container">
+
+  <div class="starter-template">
+    <h1>Bootstrap starter template</h1>
+    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+  </div>
+
+  <div class="row">
+        <div class="col-md-4">.col-md-4</div>
+        <div class="col-md-4">.col-md-4</div>
+        <div class="col-md-4">.col-md-4</div>
+    </div>
+    <a href="add.html">Add New Data</a><br/><br/>
+
+      <table width='80%' border=0>
+
+      <tr bgcolor='#CCCCCC'>
+        <td>Name</td>
+        <td>Age</td>
+        <td>Email</td>
+        <td>Update</td>
+      </tr>
+      <?php
+      //while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array
+      while($res = mysqli_fetch_array($result)) {
+        echo "<tr>";
+        echo "<td>".$res['name']."</td>";
+        echo "<td>".$res['age']."</td>";
+        echo "<td>".$res['email']."</td>";
+        echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
+      }
+      ?>
+      </table>
+
+</main><!-- /.container -->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,62 @@
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="./favicon.ico">
+<link href="https://fonts.googleapis.com/css?family=Bitter|Peddana" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="./css/style.css">
+	<title>Homepage</title>
+</head>
+<body>
+  <div class="container" id="top">
+    <p> <a href="opretbruger.php">Opret</a></p>
+	<p> <a href="login.php">Log ind</a></p>
+  </div>
+  <header class="">
+    <img src="./resources/header(edit).jpg" alt="./resources/header.jpg">
+  </header>
+  <nav class="container-fluid">
+    <ul class="row container">
+      <li class="col"><a href="#">Om Klubben</a></li>
+      <li class="col"><a href="#">Nyheder</a></li>
+      <li class="col"><a href="#">Arrangementer</a></li>
+      <li class="col"><a href="#">Galleri</a></li>
+      <li class="col"><a href="#">Bådpark</a></li>
+      <li class="col"><a href="#">Bliv Medlem</a></li>
+      <li class="col"><a href="#">Min Side</a></li>
+      <li class="col"><a href="#">Kontakt</a></li>
+    </ul>
+  </nav>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <h2>Om Klubben</h2>
+        <p>Velkommen til Kajakklubben Pagaj
+        Klubben blev stiftet i slutningen af 2015 og har lige siden oplevet en stor fremgang i medlemstallet. Vi lægger vægt på, at være en kajakklub for alle. Vi ønsker at skabe en god motionskultur og et godt fællesskab i klubben gennem hyggelige fællesture og sociale arrangementer.
+        KKP ligger i Århus Havn. Går du fra Banegården, tager det cirka 10 minutter til fods at komme til kajakklubben.
+        Århus Havn tilbyder perfekt rovand til begynderinstruktion og almindelige ture eller træning. Der er ingen store bølger i kanalen, hvilket gør det meget begyndervenligt.
+        Vi glæder os til at se dig på vandet!
+        </p>
+      </div>
+		<div class="col-md-3">
+
+		</div>
+      <div class="col-md-3 offset-md-3">
+        <h1>tesa</h1>
+      </div>
+      <div class="col-md-4">
+        <h1>test</h1>
+      </div>
+    </div>
+  </div>
+<footer class="container-fluid">
+  <div class="card-footer">
+		<div class="row">
+    	<p class="col">Kajakklubben Pagaj</p> <p class="col">Lerumvej 4</p> <p class="col">tlf: 22 22 22 22</p> <p class="col">Mail@addresse.dk</p>
+		</div>
+	</div>
+</footer>
+</body>
+</html>
